49951404cce010257852bfe907c79b8cfb3db04b
Murat Sayıcı <muratsayici@gmail.com>
2015-07-29 10:52:00 +0300

    dikdörtgen cizen python

---
+++ a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*-coding: iso-8859-9 -*-
+
+def f(e):
+    print e*" _"
+
+def g(h):
+    print (h-1)*("|"+(2*a-1)*" "+"|\n")+"|"+(a-1)*"_ "+"_"+"|"
+    print "ALAN=%s, ÇEVRE=%s" %(a*b, 2*(a+b))
+
+a=int(raw_input("yan kenar:"))
+b=int(raw_input("dik kenar:"))
+
+f(a)
+g(b)
